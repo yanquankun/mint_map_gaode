9bb0ca881ad440c1eba3d7e4f6b2508b130be2af
闫全堃 <yanquankun@golaxy.cn>
2020-07-14 19:53:03 +0800

    1.增加test文件 2.修复zh_cn语言时，地图背景空白bug 3.修改对外API名称 4.增加min文件

--- MGMap/js/MGMap.js
+++ MGMap/js/MGMap.js
@@ -53,7 +53,7 @@
             return console.error('options 参数不为对象格式');
         }
         this.zoom = 11;// 默认缩放比例
-        this.center = [116.480983, 39.989628];// 默认地图中心
+        this.center = [121.498586, 31.239637];// 默认地图中心
         this.lang = 'zh_cn'; //地图语言包 可选值：en，zh_en, zh_cn
         this.last_LngLat = null;// 暂存上次的地图中心经纬度
         if (!options || !Object.keys(options).length) {
@@ -61,6 +61,7 @@
             options.zoom = this.zoom;
             options.center = this.center;
             options.lang = this.lang;
+            options.resizeEnable = true
         }
         this._initMap(domId, options);
     }
@@ -69,39 +70,41 @@
         constructor: MGMap,
         _initMap: function (domId, options) {
             this._MGMap = new AMap.Map(domId, options);
+            console.info("version " + AMap.v)
+            return this._MGMap
         },
-        _getMapView: function () {
+        getMGMapView: function () {
             return this._MGMap;
         },
         isHaveMap: function () {
             return this._MGMap != null && this._MGMap != undefined;
         },
-        _getZoom: function () {// 获取地图缩放比例
+        getMGZoom: function () {// 获取地图缩放比例
             if (!this.isHaveMap()) return console.error('地图实例不存在');
             return this._MGMap.getZoom();
         },
-        _getLayers: function () {// 获取地图图层 
+        getMGLayers: function () {// 获取地图图层 
             if (!this.isHaveMap()) return console.error('地图实例不存在');
             return this._MGMap.getLayers();
         },
-        _getCenter: function () {// 获取地图中心
+        getMGCenter: function () {// 获取地图中心
             if (!this.isHaveMap()) return console.error('地图实例不存在');
             return this._MGMap.getCenter();
         },
-        _setMapTheme: function (type) {// 设置地图主题色
+        setMGMapTheme: function (type) {// 设置地图主题色
             // 设置主题色 
             // [标准normal 幻影黑dark 月光银light 远山黛whitesmoke 草色青fresh 雅士灰grey 
             // 涂鸦graffiti 马卡龙macaron 靛青蓝blue 极夜蓝darkblue 酱籽wine]
             if (!this.isHaveMap()) return console.error('地图实例不存在');
             this._MGMap.setMapStyle('amap://styles/' + type);
         },
-        _setCenter: function (LngLat) { // 设置中心点
+        setMGCenter: function (LngLat) { // 设置中心点
             this._MGMap.setCenter(this.last_LngLat);
         },
-        _setZoom: function (zoom) { // 设置中心点
+        setMGZoom: function (zoom) { // 设置中心点
             this._MGMap.setZoom(zoom);
         },
-        _setLang: function (lang) { // 设置地图语言
+        setMGLang: function (lang) { // 设置地图语言
             if (!this.isHaveMap()) return console.error('地图实例不存在');
             this._MGMap.setLang(lang);
         }
